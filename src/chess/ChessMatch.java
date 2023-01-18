3c2acb654305c3f8b757648bfb880a2f069143f4
ana-olima <anaclaraoliveiralima@yahoo.com.br>
2023-01-18 15:53:29 -0300

    Update Method makeMove in ChessMatch class and   modifying ChessMatch list to Piece list.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -14,8 +14,8 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
-	private List<ChessPiece> piecesOnTheBoard = new ArrayList<>();
-	private List<ChessPiece> capturedPieces = new ArrayList<>();
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 	public ChessMatch() {
 		board = new Board(8, 8);
 		turn = 1;
@@ -58,6 +58,11 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+
+		if(capturedPiece!= null){
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 
